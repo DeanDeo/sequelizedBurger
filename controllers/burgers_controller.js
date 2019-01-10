c76318b8074ea6bc45d8a6d5fe3d1180367a023e
Litsis <44249106+DeanDeo@users.noreply.github.com>
2019-01-10 13:45:58 -0600

    hopefully works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,14 @@
 var express = require("express");
 
 var router = express.Router();
-var db = require("../models");
+var burger = require("../models/burger");
 
 // get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burger");
+  res.redirect("/burgers");
 });
 
-router.get("/burger", function(req, res) {
+router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
   // burger.all(function(data) {
   //   // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
@@ -19,7 +19,7 @@ router.get("/burger", function(req, res) {
 });
 
 // post route -> back to index
-router.post("/burger/create", function(req, res) {
+router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
   // burger.create(req.body.burger_name, function(result) {
   //   // wrapper for orm.js that using MySQL insert callback will return a log to console,
@@ -37,7 +37,7 @@ router.post("/burger/create", function(req, res) {
 });
 
 // put route -> back to index
-router.put("/burger/update/:id", function(req, res) {
+router.put("/burgers/update/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
