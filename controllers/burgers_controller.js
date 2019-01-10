69ab7bd06de28503463e6751e04d1b1e4dc55b7d
Litsis <44249106+DeanDeo@users.noreply.github.com>
2019-01-10 13:40:30 -0600

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,7 @@ router.get("/burger", function(req, res) {
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
+router.post("/burger/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
   // burger.create(req.body.burger_name, function(result) {
   //   // wrapper for orm.js that using MySQL insert callback will return a log to console,
@@ -37,7 +37,7 @@ router.post("/burgers/create", function(req, res) {
 });
 
 // put route -> back to index
-router.put("/burgers/update/:id", function(req, res) {
+router.put("/burger/update/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
